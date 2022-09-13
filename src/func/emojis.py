4e2a4b5bccf872084f7a86228d54f5428caa70e4
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-09-13 13:50:39 +0900

    Create emojis.py

---
+++ src/func/emojis.py
@@ -0,0 +1,44 @@
+class emojis:
+    def one():                    return "<:one:965536298116972585>"
+    def two():                    return "<:two:965536327149953064>"
+    def three():                  return "<:three:965536346993197106>"
+    def four():                   return "<:four:965536379759128597>"
+    def five():                   return "<:five:965536400340549692>"
+
+    def good():                   return "<:good:965399379345293412>"
+    def bad():                    return "<:bad:965399278644232192>"
+
+    def channelLock():            return "<:lock:965400115886030939>"
+
+    def message():                return "<:messageO:965541428434468894>"
+    def messagePlus():            return "<:messageA:965541094043561994>"
+    def messageDouble():          return "<:messageT:965540703285420052>"
+
+    def plus():                   return "<:plus:965537792375554078>"
+    def setting():                return "<:stting:965538149755404339>"
+    def game():                   return "<:game:965402365404512307>"
+    def info():                   return "<:info:965538780431933520>"
+    def delete():                 return "<:delete:965539160788172821>"
+    def label():                  return "<:label:965539832086556712>"
+    def edit():                   return "<:edit:965540318718099506>"
+    def error():                  return "<:Error:965400603037667338>"
+    def search():                 return "<:search:981539607722225664>"
+    def star():                   return "<:star:984780323126657034>"
+    def write():                  return "<:write:984837669014085703>"
+    def coin():                   return "<:coin:1014167748248608840>"
+
+
+    def check():                  return "<:check:965401756978790430>"
+    def ax():                     return "<:ax:965401830672707745>"
+
+    def online():                 return "<:online:965396951778607134>"
+    def idle():                   return "<:idle:965396624203481128>"
+    def dnd():                    return "<:dnd:965396585406144572>"
+    def live():                   return "<:stream:965396646789787648>"
+    def offline():                return "<:offline:965397210416164874>"
+
+    def music():                  return "<:music:981533769183928330>"
+    def musicDisabled():          return "<:music_disabled:981536555128737802>"
+    def musicStop():              return "<:music_stop:981537496666079242>"
+    def musicVloumeU():           return "<:music_volume_up:981535934581456896>"
+    def musicVloumeD():           return "<:music_volume_down:981535527574581308>"
